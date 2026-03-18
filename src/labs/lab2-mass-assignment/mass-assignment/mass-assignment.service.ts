0b08882a5ea473e2fd89bb9478c01a02b585c3d5
Hotwrist <odeyjhn@gmail.com>
2026-03-18 11:03:58 +0100

    Updated auth logic and fixed JWT expiry handling

--- src/labs/lab2-mass-assignment/mass-assignment/mass-assignment.service.ts
+++ src/labs/lab2-mass-assignment/mass-assignment/mass-assignment.service.ts
@@ -16,78 +16,102 @@ import { Repository } from 'typeorm';
 
 @Injectable()
 export class MassAssignmentService {
-    constructor(
-        private labsService: LabsService, 
-        @InjectRepository(User)
-        private userRepo: Repository<User>,
-        @InjectRepository(Voucher)
-        private voucherRepo: Repository<Voucher>
-    ) {}
+  constructor(
+    private labsService: LabsService,
+    @InjectRepository(User)
+    private userRepo: Repository<User>,
+    @InjectRepository(Voucher)
+    private voucherRepo: Repository<Voucher>
+  ) {}
+  async updateUser(userId: number, reqBody: UpdateUserDto, headers: any) {
+    let voucherSolved = false;
+    //const user = await this.userRepo.findOne({ where:{ id: userId }});
 
-    async updateUser(userId: number, reqBody: UpdateUserDto, headers: any) {
-        let voucherSolved = false;
-        //const user = await this.userRepo.findOne({ where:{ id: userId }});
+    // I need the hidden voucherCode that i set in the database not to be exposed, to be
+    // exposed when a user solves the voucherCode lab, hence the reason i am making use of .createQueryBuilder()
+    const user = await this.userRepo
+      .createQueryBuilder('user')
+      .addSelect('user.voucherCode')
+      .where('user.id = :id', { id: userId })
+      .getOne();
 
-        // I need the hidden voucherCode that i set in the database not to be exposed, to be
-        // exposed when a user solves the voucherCode lab, hence the reason i am making use of .createQueryBuilder()
-        const user = await this.userRepo
-        .createQueryBuilder("user")
-        .addSelect("user.voucherCode")
-        .where("user.id = :id", { id: userId })
-        .getOne();
+    if (!user) return { message: `User with user id ${userId} not found!` };
 
-        if(!user) return { message: `User with user id ${userId} not found!` }
+    // Vulnerability: Accepting arbitrary fields
+    Object.assign(user, reqBody);
+    await this.userRepo.save(user);
 
-        // Vulnerability: Accepting arbitrary fields
-        Object.assign(user, reqBody);
-        await this.userRepo.save(user);
-
-        // Task detection
-        if (reqBody.role) {
-            user.role = (reqBody.role === "admin") ? "user" : reqBody.role;
-            await this.labsService.markTaskSolved(userId, "Mass Assignment", "Modify your own role");
-        }
-
-        if (reqBody.role === "admin" && headers['x-admin-override'] == "true") {
-            user.role = "admin";
-            await this.labsService.markTaskSolved(userId, "Mass Assignment", "Become an admin");
-        }
-        
-        if (reqBody.isVerified) {
-            user.isVerified = reqBody.isVerified;
-            await this.labsService.markTaskSolved(userId, "Mass Assignment", "Modify protected account fields (Become Verified)");
-        }
-
-        if(reqBody.isPremium) { user.isPremium = false; }
-
-        if (reqBody.voucherCode ) {
-            const voucher = await this.voucherRepo.findOne({ where: {code: reqBody.voucherCode}});
-
-            if(!voucher) return { message: `Invalid voucher: ${reqBody.voucherCode}` }
-
-            user.voucherCode = reqBody.voucherCode;
-            
-            await this.labsService.markTaskSolved(userId, "Mass Assignment", "Modify protected account fields (Become Premium)");
-
-            voucherSolved = true;
-        }
-        await this.userRepo.save(user);
-
-        const response: any = {
-            message: "Profile updated!",
-            user: {
-                id: user.id,
-                email: user.email,
-                role: user.role,
-                isVerified: user.isVerified
-            }
-        };
-
-        if(voucherSolved === true) {
-            response.message = "🎉 Voucher unlocked!";
-            response.user.voucherCode = user.voucherCode;
-        }
-        
-        return response;
+    // Task detection
+    if (reqBody.role) {
+      user.role = reqBody.role === 'admin' ? 'user' : reqBody.role;
+      await this.labsService.markTaskSolved(
+        userId,
+        'Mass Assignment',
+        'Modify your own role',
+      );
     }
+
+    if (reqBody.role === 'admin' && headers['x-admin-override'] == 'true') {
+      user.role = 'admin';
+      await this.labsService.markTaskSolved(
+        userId,
+        'Mass Assignment',
+        'Become an admin',
+      );
+    }
+
+    if (reqBody.isVerified) {
+      user.isVerified = reqBody.isVerified;
+      await this.labsService.markTaskSolved(
+        userId,
+        'Mass Assignment',
+        'Modify protected account fields (Become Verified)'
+      );
+    }
+
+    if (reqBody.isPremium) {
+      user.isPremium = false;
+    }
+
+    if (reqBody.voucherCode) {
+      const voucher = await this.voucherRepo.findOne({
+        where: {
+          code: reqBody.voucherCode,
+        },
+      });
+
+      if (!voucher)
+        return {
+          message: `Invalid voucher: ${reqBody.voucherCode}`,
+        };
+      user.voucherCode = reqBody.voucherCode;
+
+      await this.labsService.markTaskSolved(
+        userId,
+        'Mass Assignment',
+        'Modify protected account fields (Become Premium)',
+      );
+
+      voucherSolved = true;
+      user.isPremium = true;
+    }
+    await this.userRepo.save(user);
+
+    const response = {
+      message: 'Profile updated!',
+      user: {
+        id: user.id,
+        email: user.email,
+        role: user.role,
+        isVerified: user.isVerified,
+        voucherCode: user.voucherCode,
+      },
+    };
+
+    if (voucherSolved === true) {
+      response.message = '🎉 Voucher unlocked!';
+      response.user.voucherCode = user.voucherCode;
+    }
+    return response;
+  }
 }
